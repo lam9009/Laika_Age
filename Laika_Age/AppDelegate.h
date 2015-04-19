1d48c83ee4db58198f36e258e7ef8e1c33ca56fc
Alfred <Alfred@Alfreds-MacBook-Pro.local>
2015-04-19 14:24:22 -0700

    Initial Commit

---
+++ Laika_Age/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Laika_Age
+//
+//  Created by Alfred on 4/19/15.
+//  Copyright (c) 2015 maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Laika_Age/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Laika_Age
+//
+//  Created by Alfred on 4/19/15.
+//  Copyright (c) 2015 maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
